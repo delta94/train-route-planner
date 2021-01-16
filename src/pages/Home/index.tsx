97039b4d590b7902630d61e584154472f0997ace
Gianfranco F H <gianffh@gmail.com>
2021-01-17 00:49:38 +0800

    Put together Input source and Input destination into home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
 
+// Components
+import InputSelect from '../../components/InputSelect';
+
 // Utils;
 import { fetchGET } from '../../utils/fetch';
 import pathFinder from '../../utils/pathFinder';
-import { formatResultsAsInstruction } from '../../utils/formatData';
+import {
+  formatResultsAsInstruction,
+  getStationAndTheLines,
+} from '../../utils/formatData';
+
+// Assets
+import { ReactComponent as Target } from '../../assets/icons/target.svg';
+import { ReactComponent as Dot } from '../../assets/icons/dot.svg';
+import { ReactComponent as MapPin } from '../../assets/icons/map-pin.svg';
 
 // Types
-import { StationData } from '../../types/station';
+import { StationData, StationName } from '../../types/station';
+
+import styles from './Home.module.scss';
 
 function Home() {
   const [isError, setIsError] = React.useState<boolean>(false);
   const [stationData, setStationData] = React.useState<StationData>();
+  const [source, setSource] = React.useState<StationName>('');
+  const [destination, setDestination] = React.useState<StationName>('');
 
   React.useEffect(() => {
     const getData = async () => {
@@ -33,7 +48,40 @@ function Home() {
     }
   }, [stationData]);
 
-  return <div>hello</div>;
+  const listOfStations = React.useMemo(
+    () => getStationAndTheLines(stationData ?? {}),
+    [stationData]
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.labelIcons}>
+        <Target className={styles.icon} />
+        <Dot className={styles.icon} />
+        <Dot className={styles.icon} />
+        <MapPin className={styles.icon} />
+      </div>
+      <div className={styles.form}>
+        <InputSelect
+          className={styles.inputFrom}
+          placeholder="Choose starting point"
+          options={Object.entries(listOfStations).map(([station]) => ({
+            value: station,
+            label: station,
+          }))}
+          onSelect={(value) => setSource(value)}
+        />
+        <InputSelect
+          placeholder="Choose destination"
+          options={Object.entries(listOfStations).map(([station]) => ({
+            value: station,
+            label: station,
+          }))}
+          onSelect={(value) => setDestination(value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
