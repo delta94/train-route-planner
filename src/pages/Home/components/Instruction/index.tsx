ba60863d582e2d38bf90c0f2da6d48c69ddff189
Gianfranco F H <gianffh@gmail.com>
2021-01-17 23:28:44 +0800

    Add UI for instruction details

--- src/pages/Home/components/Instruction/index.tsx
+++ src/pages/Home/components/Instruction/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 
 // Components
 import LineBadge from 'components/LineBadge';
+
+// Constants
+import { LINE_TO_COLOR } from 'constants/lineColor';
+
 // Assets
 import { ReactComponent as ArrowRight } from 'assets/icons/arrow-right.svg';
 
@@ -25,22 +29,44 @@ function InstructionComponent({ instruction, onClick }: Props) {
           {linesTaken.map((line, idx) => (
             <span key={line} className={styles.line}>
               {idx > 0 && <ArrowRight className={styles.arrowRightIcon} />}
-              <LineBadge className={styles.lineBadge2} line={line} />
+              <LineBadge className={styles.lineBadge} line={line} />
             </span>
           ))}
         </div>
         <p className={styles.numOfStops}>{numOfStopsInTotal} stops</p>
       </div>
       {showDetail && (
-        <div>
+        <div className={styles.instructionDetail}>
           {detail.map(
             ({ initialStation, finalStation, numOfStops, lineTaken }, idx) => (
-              <div key={idx}>
-                <div>{initialStation}</div>
-                <div>{finalStation}</div>
-                <div>{numOfStops}</div>
-                <div>{lineTaken}</div>
-              </div>
+              <React.Fragment key={idx}>
+                {idx > 0 && (
+                  <div className={styles.changeLine}>
+                    Change line
+                    <LineBadge
+                      className={styles.changeLineBadge}
+                      line={detail[idx - 1].lineTaken}
+                    />
+                    to
+                    <LineBadge
+                      className={styles.changeLineBadge}
+                      line={lineTaken}
+                    />
+                  </div>
+                )}
+                <div className={styles.detailStep}>
+                  <div
+                    className={styles.connectorLine}
+                    style={{ backgroundColor: LINE_TO_COLOR[lineTaken] }}
+                  />
+                  <div className={styles.stationName}>{initialStation}</div>
+                  <div className={styles.stepNumOfStops}>
+                    <LineBadge className={styles.lineBadge} line={lineTaken} />
+                    <span>{numOfStops} stops</span>
+                  </div>
+                  <div className={styles.stationName}>{finalStation}</div>
+                </div>
+              </React.Fragment>
             )
           )}
         </div>
